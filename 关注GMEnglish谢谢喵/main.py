93c870b79873b0522233810b74ae766bb6285758
FBIMAYO@已格式化 <125355822+FBIMAY0@users.noreply.github.com>
2023-10-22 18:35:19 +0800

    Create main.py

---
+++ 关注GMEnglish谢谢喵/main.py
@@ -0,0 +1,14 @@
+import time
+import random
+import ctypes
+
+
+def msgbox(message="关注GMEnglish喵，关注GMEnglish谢谢喵", title="广告"):
+    ctypes.windll.user32.MessageBoxW(0, message, title, 0x40)
+
+
+while True:
+    waitSecond = random.randint(60, 600)
+    print(''.join(['[INFO] 距离下次弹窗还剩：', str(waitSecond), ' 秒']))
+    time.sleep(waitSecond)
+    msgbox()
